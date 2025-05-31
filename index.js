6a207b5480379c2b5d7371ac3f2a687768650ae3
Olive Njoroge <njorogeolive001@gmail.com>
2025-05-31 22:52:09 +0300

    Fired up the app and commented code

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-//Create an express server
+//Create an express server and Middleware
 const app = express();
-
-//Create Middleware
 app.use(express.json());
 
+//Constants
 const mongoUri = 'mongodb://localhost:27017/taskdb';
 const PORT = 3000;
 
+//mongoDB connection
 mongoose.connect(mongoUri, {
     useNewUrlParser : true,
     useUnifiedTopology : true
-}).then(() => console.log('Connected'))
+}).then(() => console.log('Connected to mongoDB'))
   .catch(err => console.error('MongoDb connection error:', err));
 
-//Listener
+//Listener / fire up the server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
 });
