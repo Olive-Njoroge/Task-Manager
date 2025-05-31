b40d6d88d19dcbcda00dc2df75d326cf1af4a7f4
Olive Njoroge <njorogeolive001@gmail.com>
2025-05-31 22:28:41 +0300

    Create a connection between mongoDb and mongoose

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const mongoose = require('mongoose');
 
 //Create an express server
 const app = express();
+
+//Create Middleware
+app.use(express.json());
+
+const mongoUri = 'mongodb://localhost:27017/taskdb';
+const PORT = 3000;
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(() => console.log('Connected'))
+  .catch(err => console.error('MongoDb connection error:', err));
+
+//Listener
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`)
+});
