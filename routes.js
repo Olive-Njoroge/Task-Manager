b5d57aee7077bfc786a3ca8a41988a50a036c629
Olive Njoroge <njorogeolive001@gmail.com>
2025-06-01 08:35:44 +0300

    added code to delete tasks by id

--- routes.js
+++ routes.js
@@ -49,4 +49,15 @@ router.put('/tasks/:id', async(req, res) => {
     }
 });
 
+//Delete a task
+router.delete('/tasks/:id', async(req,res) => {
+    try{
+        const task = await Task.findByIdAndDelete((req.params.id));
+        if(!task) return res.status(404).send();
+        res.send(task);
+    }catch(error){
+        res.status(500).send(error);
+    }
+});
+
 module.exports = router;
